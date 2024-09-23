ae8b58346ec1a8afdd3b53186af7149a0e2c4a7a
Maxim Zubchenko <mzubchenko@callfire.com>
2024-09-23 22:28:48 +0200

    merge new-strapi

--- config/plugins.js
+++ config/plugins.js
@@ -1,11 +1,1 @@
-module.exports = ({ env }) => ({
-  email: {
-    config: {
-      provider: "sendmail",
-      settings: {
-        defaultFrom: "petrushkapapasov3@yandex.ru",
-        defaultReplyTo: "petrushkapapasov3@yandex.ru",
-      },
-    },
-  },
-});
+module.exports = () => ({});
